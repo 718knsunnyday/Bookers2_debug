439ddf73ede0d38fdecf922dfdabe376736a480d
718knsunnyday <718mickeeey@gmail.com>
2021-10-29 03:37:42 +0000

    fixcomment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,10 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
     @book=Book.find(params[:book_id])
     @book_comment=BookComment.new(book_comment_params)
     @book_comment.user_id=current_user.id
-    @book_comment.book_book_id=@book.id
+    @book_comment.book_id=@book.id
     if @book_comment.save
       redirect_to book_path(@book.id)
     else
@@ -20,8 +20,7 @@ class BookCommentsController < ApplicationController
 
   private
 
-  def post_comment_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
 end
-
